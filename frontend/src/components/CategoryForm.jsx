a9c879eb9bde481976ce557fc1d2743e6b90e7d3
vluca997 <vluca997@gmail.com>
2024-05-22 15:24:56 +0200

    Add Category create,updated,delete

---
+++ frontend/src/components/CategoryForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const CategoryForm = ({
+    value,
+    setValue,
+    handleSubmit,
+    buttonText = 'Submit',
+    handleDelete
+}) => {
+    return (
+        <div className="p-3">
+            <form onSubmit={handleSubmit} className='space-y-3'>
+                <input 
+                    type="text" 
+                    className='py-3 px-4 border rounded-lg w-full' 
+                    placeholder='Scrivi il nome della categoria: ' 
+                    value={value} 
+                    onChange={(e) => setValue(e.target.value)} />
+                <div className='flex justify-between'>
+                    <button className='bg-pink-500 hover:bg-pink-700 text-white py-2 px-4 rounded-lg 
+                                        focus:outline-none focus:ring-2 focus:ring-pink-500  focus:ring-opacity-50'>
+                        {buttonText}
+                    </button>
+                    {handleDelete && (
+                        <button 
+                            onClick={handleDelete}
+                            className='bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 focus:outline-none 
+                                        focus:ring-2 focus:ring-red-500 focus:ring-opacity-50'>
+                            Elimina Categoria:
+                        </button>
+                    )}
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default CategoryForm
--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -1,10 +1,130 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { toast } from 'react-toastify'
-
-
+import { useCreateCategoryMutation, useDeleteCategoryMutation, useUpdateCategoryMutation, useFetchCategoriesQuery } from '../../redux/api/categoryApiSlice'
+import Modal from '../../components/Modal.jsx'
+import CategoryForm from '../../components/CategoryForm.jsx'
 
 export const CategoryList = () => {
-  return (
-    <div>CategoryList</div>
-  )
+    const { data: categories } = useFetchCategoriesQuery()
+    console.log(categories)
+
+    const [name, setName] = useState('')
+    const [selectedCategory, setSelectedCategory] = useState(null)
+    const [updatingName, setUpdatingName] = useState('')
+    const [modalVisible, setModalVisible] = useState(false)
+
+    const [createCategory] = useCreateCategoryMutation()
+    const [updateCategory] = useUpdateCategoryMutation()
+    const [deleteCategory] = useDeleteCategoryMutation()
+
+
+    
+    const handleCreateCategory = async (e) => {
+        e.preventDefault()
+
+        if (!name) {
+            toast.error('Il nome della categoria è obbligatorio!')
+            return
+        }
+
+        try {
+            const result = await createCategory({ name }).unwrap()
+            if (result.error) {
+                toast.error(result.error)
+            } else {
+                setName("")
+                toast.success(`La categoria ${result.name} è stata creata`)
+            }
+        } catch (error) {
+            console.error(error)
+            toast.error('Creazione della categoria fallita, riprova!')
+        }
+    }
+
+
+
+
+
+    const handleUpdateCategory = async (e) =>{
+        e.preventDefault()
+
+        if(!updatingName){
+            toast.error('Il Nome della categoria è obbligatorio')
+            return
+        }
+        try {
+            
+            const result = await updateCategory({categoryId: selectedCategory._id, updateCategory:{
+                name: updatingName
+            }}).unwrap();
+
+            if(result.error){
+                toast.error(result.error)
+            }else{
+                toast.success(`${result.name} è stato modificato!`)
+                setSelectedCategory('')
+                setModalVisible(false)
+            }
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
+
+    const handleDeleteCategory = async () =>{
+        try {
+            const result = await deleteCategory(selectedCategory._id).unwrap();
+
+            if(result.error){
+                toast.error(result.error)
+            }else{
+                toast.success(`${result.name} è stato cancellato`)
+                setSelectedCategory(null)
+                setModalVisible(false)
+            }
+        } catch (error) {
+            console.error(error);
+            toast.error("Eliminazione della categoria fallita, riprova")
+        }
+    }
+
+
+
+    return (
+        <div className="flex flex-col md:flex-row ml-[10rem]">
+            <div className="md:w-3/4 p-3">
+                <div className="h-12">Gestione Categorie</div>
+                <CategoryForm value={name} setValue={setName} handleSubmit={handleCreateCategory} />
+                <br />
+                <hr />
+                <div className="flex flex-wrap">
+                    {categories?.map((category) => (
+                        <div key={category._id} className='px-4 py-4'>
+                            <button
+                                className='bg-white hover:bg-pink-500 borde-pink-500 text-pink-500 hover:text-white py-2 px-4 m-1 rounded-lg 
+                                            focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50'
+                                onClick={() => {
+                                    setModalVisible(true)
+                                    setSelectedCategory(category)
+                                    setUpdatingName(category.name)
+                                }}
+                            >
+                                {category.name}
+                            </button>
+                        </div>
+                    ))}
+                </div>
+                <Modal isOpen={modalVisible} onClose={() => setModalVisible(true)}>
+                    <CategoryForm 
+                        value={updatingName} 
+                        setValue={value => setUpdatingName(value)} 
+                        handleSubmit={handleUpdateCategory} 
+                        buttonText='Update' 
+                        handleDelete={handleDeleteCategory}/>
+                </Modal>
+            </div>
+        </div>
+    )
 }
