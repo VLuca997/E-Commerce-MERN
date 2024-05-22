4f3862d1d48e3f50fb3fdb32ceabd9ff72ab97d0
vluca997 <vluca997@gmail.com>
2024-05-22 12:21:52 +0200

    FRONTEND: RESTapi CATEGORY, routing

---
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -0,0 +1,10 @@
+import React,{useState} from 'react'
+import { toast } from 'react-toastify'
+
+
+
+export const CategoryList = () => {
+  return (
+    <div>CategoryList</div>
+  )
+}
---
+++ frontend/src/redux/api/categoryApiSlice.js
@@ -0,0 +1,43 @@
+import { apiSlice } from "./apiSlice";
+import { CATEGORY_URL } from "../constants";
+
+export const categoryApiSlice = apiSlice.injectEndpoints({
+    endpoints:(builder) =>({
+        createCategory: builder.mutation({
+            query: (newCategory) =>({
+                url: `${CATEGORY_URL}`,
+                method: "POST",
+                body: newCategory,
+            })
+        }),
+        updateCategory: builder.mutation({
+            query: ({categoryId, updateCategory}) =>({
+                url: `${CATEGORY_URL}/${categoryId}`,
+                method: "PUT",
+                body: updateCategory,
+            })
+        }),
+        deleteCategory: builder.mutation({
+            query: (categoryId) =>({
+                url: `${CATEGORY_URL}/${categoryId}`,
+                method: "DELETE",
+            })
+        }),
+
+
+        fetchCategories: builder.query({
+            query: () => `${CATEGORY_URL}/categories`
+        })
+    })
+})
+
+
+
+export const {
+    useCreateCategoryMutation,
+    useDeleteCategoryMutation,
+    useUpdateCategoryMutation,
+    useFetchCategoriesQuery
+} = categoryApiSlice
+
+
